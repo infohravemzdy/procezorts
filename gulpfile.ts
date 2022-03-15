40dfe6e8bb5f34ac381d3f322ff27162bd01a2d4
mzdyhrave <info@hravemzdy.org>
2022-03-15 12:22:21 +0100

    Release 0.22.15.

--- gulpfile.ts
+++ gulpfile.ts
@@ -6,8 +6,10 @@ const compile = () => src('package.json').pipe(shell('npm run compile'));
 
 const copy = () => src(['README.md']).pipe(dest('dist'));
 
+const copyTypes = () => src(['types/**/*']).pipe(dest('dist'));
+
 const update = () => src('package.json').pipe(replace('"private": true,', '"private": false,')).pipe(dest('dist'));
 
 const publish = () => src('package.json').pipe(shell('cd dist && npm publish'));
 
-export default series(compile, copy, update, publish);
+export default series(compile, copy, copyTypes, update, publish);
